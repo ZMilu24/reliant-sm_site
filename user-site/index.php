f183761b9c56e92df1fadee1505182f989a6209c
ZMilan24 <93102357+ZMilu24@users.noreply.github.com>
2024-03-28 21:14:53 +0100

    main_page

--- user-site/index.php
+++ user-site/index.php
@@ -1,9 +1,38 @@
 <?php
-
     require_once("header.php");
+    require_once("functions.php");
+?>
 
-    require_once("main_page.php");
+<main class="row text-center p-5 text-white">
+    <?php
+        //A sorok számánál oda kell figyelni a bootstrap 5 szintaktikára is!!!
+        card_in_row([
+            "Costum" => [
+                "name" => "Costum price",
+                "details" => [
+                    "asd" => "asd",
+                    "asd" => "asd"
+                ]
+            ],
+            "olcso" => [
+                "name" => "100$",
+                "details" => [
+                    "asd" => "asd",
+                    "asd" => "asd",
+                    "asd" => "asd"
+                ]
+            ],
+            "draga" => [
+                "name" => "200$",
+                "details" => [
+                    "asd" => "asd",
+                    "asd" => "asd"
+                ]
+            ]
+        ], "price");
+    ?>
+</main>
 
+<?php
     require_once("footer.php");
-
 ?>
