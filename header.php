be4cdf3f9ee05164a08ab43561794eb9aa9fb69c
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-04-08 11:37:31 +0200

    packages

--- header.php
+++ header.php
@@ -7,7 +7,7 @@ function header_fnc($color="868686") { ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Reliant Service Management</title>
 </head>
 <body>
 <header class="container-fluid" style="background-image: linear-gradient(to right, white, #<?php echo($color); ?>);">
--- package.php
+++ package.php
@@ -5,23 +5,174 @@
     $themes = array();
     $themes["basic"] = [
         "name" => "Basic",
-        "Main price" => "150",
+        "main price" => "150",
+        "month" => "123",
+        "half-year" => "345",
+        "year" => "567",
         "theme color" => "9AEBB1"
     ];
-    $themes["premium"] = [];
+    $themes["premium"] = [
+        "name" => "Premium",
+        "main price" => "350",
+        "month" => "123",
+        "half-year" => "345",
+        "year" => "567",
+        "theme color" => "DBF5F5"
+    ];
+    $themes["ultra"] = [
+        "name" => "Ultra",
+        "main price" => "500<...",
+        "month" => "123",
+        "half-year" => "345",
+        "year" => "567",
+        "warning" => "szöveg",
+        "theme color" => "424478"
+    ];
 
     $package = null;
 
-    if ($_GET["p"] = "basic") {
+    if ($_GET["p"] == "b") {
         $package = $themes["basic"];
+    } else if ($_GET["p"] == "p") {
+        $package = $themes["premium"];
+    } else if ($_GET["p"] == "u") {
+        $package = $themes["ultra"];
     }
 
 ?>
 
     <?php header_fnc($package["theme color"])?>
 
-    <main>
-
+    <main class="container-fluid" style="background-image: linear-gradient(to bottom right, white, #<?php echo($package["theme color"]); ?>);">
+    <div class="row p-3 px-5">
+        <div class="col text-left">
+            <h1><i><?php echo($package["name"])?></i></h1>
+        </div>
+    </div>
+    <div class="row px-5">
+        <div class="col text-left">
+            <h2>$<?php echo($package["main price"])?></h2>
+        </div>
+    </div>
+    <div class="row p-3 px-5 mx-5">
+        <div class="col text-left">
+            <div class="row">
+                <div class="col text-left">
+                    <p>cui dixit Dominus unde venis qui respondens ait circuivi terram et perambulavi eam</p>
+                </div>
+                <div class="col text-left">
+                    <div class="row">
+                        <div class="col text-left">
+                            <ul>
+                                <li>Havi</li>
+                                <li>Féléves</li>
+                                <li>Éves</li>
+                            </ul>
+                        </div>
+                        <div class="col text-left">
+                            <div class="row px-5">
+                                <div class="col text-center">
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <tr>
+                                                <td class="text-center bg-light">
+                                                    <div class="m-3 my-1">Havi</div>
+                                                </td>
+                                                <td class="text-center bg-light">
+                                                    <div class="m-3 my-1">Féléves</div>
+                                                </td>
+                                                <td class="text-center bg-light">
+                                                    <div class="m-3 my-1">Éves</div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td class="text-center bg-light">
+                                                    <div class="m-3 my-1">$<?php echo($package["month"])?></div>
+                                                </td>
+                                                <td class="text-center bg-light">
+                                                    <div class="m-3 my-1">$<?php echo($package["half-year"])?></div>
+                                                </td>
+                                                <td class="text-center bg-light">
+                                                    <div class="m-3 my-1">$<?php echo($package["year"])?></div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col text-center">
+                                    <p class="text-danger">dixitque Dominus ad eum numquid considerasti servum meum Iob quod non sit ei similis in terra homo simplex et rectus et timens Deum ac recedens a malo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="package_main">
+                <div class="row">
+                    <div class="col">
+                        <p>
+                            Ábelesz
+                            kábelesz
+                            rákmegesz
+                        </p>
+                        <p>
+                            Ábelesz
+                            kábelesz
+                            rákmegesz
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>
+                            Ábelesz
+                            kábelesz
+                            rákmegesz
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>
+                            <b>
+                                Ábelesz
+                                kábelesz
+                                rákmegesz
+                            </b>
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>
+                            Ábelesz
+                            kábelesz
+                            rákmegesz
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="text-danger"><?php if ($_GET["p"] == "u") { echo($package["warning"]); }?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-end mx-5 px-5">
+                    <button type="button" class="btn btn-dark btn-lg">
+                        <b class="w-100">Sablon megtekintése</b>
+                    </button>
+                </div>
+                <div class="col text-left mx-5 px-5">
+                    <button type="button" class="btn btn-dark btn-lg">
+                        <b class="w-100">Fizetés</b>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
     </main>
 
     <?php footer_fnc()?>
