b05256e6c4904d6dd17e643c1a99c6922e3f7546
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-04-28 21:56:35 +0200

    footer fix

--- admin/index.php
+++ admin/index.php
@@ -1,64 +1,167 @@
-<?php
-    require_once("main_site.php");
-
-    if (isset($_POST["Login"])) {
-        if ($_POST["un"] == "Reliant_admin-user" and $_POST["pw"] == "Ba_Zu_189672") {
-            $_SESSION["logged_in_admin"] = true;
-        } else {
-            $_SESSION["logged_in_admin"] = false;
-        }
-    }
-
-?>
 <!DOCTYPE html>
-<html lang="hu">
+<html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <title>Reliant-admin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
-            table {
-                border-collapse: collapse;
+            body {font-family: Arial, Helvetica, sans-serif;}
+
+            /* Full-width input fields */
+            input[type=text], input[type=password] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+            }
+
+            /* Set a style for all buttons */
+            button {
+            cursor: pointer;
+            }
+
+            button:hover {
+            opacity: 0.8;
+            }
+
+            /* Extra styles for the cancel button */
+            .cancelbtn {
+            width: auto;
+            padding: 10px 18px;
+            background-color: #f44336;
+            }
+
+            /* Center the image and position the close button */
+            .imgcontainer {
+            text-align: center;
+            margin: 24px 0 12px 0;
+            position: relative;
+            }
+
+            img.avatar {
+            width: 40%;
+            border-radius: 50%;
+            }
+
+            .container {
+            padding: 16px;
+            }
+
+            span.psw {
+            float: right;
+            padding-top: 16px;
+            }
+
+            /* The Modal (background) */
+            .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+            padding-top: 60px;
+            }
+
+            /* Modal Content/Box */
+            .modal-content {
+            background-color: #fefefe;
+            margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+            border: 1px solid #888;
+            width: 80%; /* Could be more or less, depending on screen size */
+            }
+
+            /* The Close Button (x) */
+            .close {
+            position: absolute;
+            right: 25px;
+            top: 0;
+            color: #000;
+            font-size: 35px;
+            font-weight: bold;
+            }
+
+            .close:hover,
+            .close:focus {
+            color: red;
+            cursor: pointer;
+            }
+
+            /* Add Zoom Animation */
+            .animate {
+            -webkit-animation: animatezoom 0.6s;
+            animation: animatezoom 0.6s
+            }
+
+            @-webkit-keyframes animatezoom {
+            from {-webkit-transform: scale(0)} 
+            to {-webkit-transform: scale(1)}
+            }
+            
+            @keyframes animatezoom {
+            from {transform: scale(0)} 
+            to {transform: scale(1)}
+            }
+
+            /* Change styles for span and cancel button on extra small screens */
+            @media screen and (max-width: 300px) {
+            span.psw {
+                display: block;
+                float: none;
+            }
+            .cancelbtn {
                 width: 100%;
             }
-
-            th, td {
-                text-align: left;
-                padding: 8px;
-            }
-
-            tr:nth-child(even){background-color: #d0d0d0}
-
-            th {
-                background-color: #0aab25;
-                color: white;
             }
         </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
-    <body>
-    <?php
-    
-        if ($_SESSION["logged_in_admin"] == true) {
-            main();
-        } else {
-            ?>
-            
-            <form method="post">
+    <body class="container bg-dark">
 
-                <label>Username</label><input type="text" placeholder="username" name="un">
-                <br />
-                <br />
-                <label>Password</label><input type="password" placeholder="password" name="pw">
-                <br />
-                <br />
-                <input type="submit" name="Login" value="Login">
+        <div class="row">
+            <div class="col m-5 text-center bg-dark">
 
-            </form>
+                <button class="btn btn-primary" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
 
-            <?php
-        }
+                <div id="id01" class="modal bg-dark">
+                    
+                    <form class="modal-content animate w-50 bg-dark" action="/action_page.php" method="post">
+                        <div class="imgcontainer">
+                        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                        </div>
 
-    ?>
+                        <div class="container bg-dark">
+                        <label class="text-primary" for="uname"><b>Username</b></label>
+                        <input class="bg-dark" type="text" placeholder="Enter Username" name="uname" required>
+
+                        <label class="text-primary" for="psw"><b>Password</b></label>
+                        <input class="bg-dark" type="password" placeholder="Enter Password" name="psw" required>
+                            
+                        <button type="submit" class="btn btn-primary">Login</button>
+
+                        <div class="container bg-dark text-start" style="background-color:#f1f1f1">
+                            <button class="btn btn-danger" type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            // Get the modal
+            var modal = document.getElementById('id01');
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
+
     </body>
 </html>
--- footer.php
+++ footer.php
@@ -1,11 +1,14 @@
 <?php
+
+require("config.php");
+
 function footer_fnc($color = "868686") { ?>
     <footer class="container-fluid p-0" style="background-image: linear-gradient(to left, white, #<?php echo($color); ?>);">
         <div class="container">
             <div class="row w-100">
                 <div class="col p-3 px-5">
                     <div class="row">
-                        <div class="col text-left">
+                        <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
                             <h4>
                                 <b id="footer_home">Reliant Service Management</b>
                             </h4>
@@ -13,16 +16,16 @@ function footer_fnc($color = "868686") { ?>
                         <!---->
                         <div class="col text-center">
                             <div class="row">
-                                <div class="col text-left">
-                                    <div class="row text-left">
-                                        <div class="col text-left">
+                                <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
+                                    <div class="row <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
+                                        <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
                                             <h4>
                                                 E-mail
                                             </h4>
                                         </div>
                                     </div>
-                                    <div class="row text-left">
-                                        <div class="col text-left">
+                                    <div class="row <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
+                                        <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
                                             <p>
                                                 <a id="email_link" onclick=copy(id) class="footer-link">reliant.service.management@gmail.com</a>
                                             </p>
@@ -35,7 +38,7 @@ function footer_fnc($color = "868686") { ?>
                 </div>
                 <div class="col p-3 px-5">
                     <div class="row">
-                        <div class="col text-end">
+                        <div class="col <?php if($isMob) {echo("text-end");} else {echo("text-center");} ?>">
                             <div class="row">
                                 <div class="col">
                                     <h4>
@@ -52,7 +55,7 @@ function footer_fnc($color = "868686") { ?>
                             </div>
                         </div>
                         <!---->
-                        <div class="col text-end">
+                        <div class="col <?php if($isMob) {echo("text-end");} else {echo("text-center");} ?>">
                             <img src="imgs/logo2.png" class="rounded-circle" height="100">
                         </div>
                     </div>
