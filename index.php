34819559199efd47705f23a2a3bbee9133045b61
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-05-06 21:13:42 +0200

    saved

--- index.php
+++ index.php
@@ -42,38 +42,36 @@
                             <h2>A webfejlesztés építőkövei</h2>
                         </div>
                     </div>
-                    <div class="row pt-3">
-                        <div class="col text-center my-3">
-                            <?php info_card("Tervezés", "Csapatunk az Ön által megadott indormációk alapján, a lehető legmegfelelőbb weboldaldesign-t
-                                                         készíti el, vagy kézzel a jobb minőség érdekében, vagypedig már létező sablon alapján
-                                                         (Az imént említett két lehetőség az oldalunk további részeiben még említve lesz).") ?>
-                        </div>
-                        <div class="col text-center my-3">
-                            <?php info_card("Webdesign", 'Cégünk design-ért felelős részlege, a "Tervezés" részben már említett módokon, élénk, élettelteli és 
-                                                          a lehető legkorszerűbb kinézetre törekszik, hogy Önnek, mint Ügyfél a tőlünk telhető legjobbat nyújtsa.
-                                                          Minden weboldal, mely nálunk készül el, szakképzett, és tapasztalt munkatársaink bizonyosodnak meg róla,
-                                                          hogy az ajánlattételben elvártakból, a legjobbat hozzuk ki (Az ajánlattételről szó esik a "Kínálatunk" oldalunkon).') ?>
-                        </div>
+                </div>
+                <div class="row pt-3">
+                    <div class="col text-center my-3">
+                        <?php info_card("Tervezés", "Csapatunk az Ön által megadott indormációk alapján, a lehető legmegfelelőbb weboldaldesign-t
+                                                        készíti el, vagy kézzel a jobb minőség érdekében, vagypedig már létező sablon alapján
+                                                        (Az imént említett két lehetőség az oldalunk további részeiben még említve lesz).") ?>
                     </div>
-                    <div class="row pt-3">
-                        <div class="col text-center my-3">
-                            <?php info_card("SEO", "Az általunk készített weboldalakat, a Google keresőmotorjához optimalizáljuk,
-                                                    így az Önt sokkal hamarabb találják meg a potenciális látogatók.") ?>
-                        </div>
-                        <div class="col text-center my-3">
-                            <?php info_card("Webfejlesztés", "Cégünk fejlesztésért felelős részlege, a design osztályról megérkezett minta alapján, a weoldalt életre kelti.
-                                                              A programozás minden apró kis szegleteiben jártas munkatársaink, ötletes megoldásokkal igyekeznek az Ön weboldalát, mind funkciók,
-                                                              mind multikompatibilitás tekintetében tökéletessé tenni. Programozóink készségei lefedik az egész ipart, kezdve a javascript-től a php-ig.") ?>
-                        </div>
+                    <div class="col text-center my-3">
+                        <?php info_card("Webdesign", 'Cégünk design-ért felelős részlege, a "Tervezés" részben már említett módokon, élénk, élettelteli és 
+                                                        a lehető legkorszerűbb kinézetre törekszik, hogy Önnek, mint Ügyfél a tőlünk telhető legjobbat nyújtsa.
+                                                        Minden weboldal, mely nálunk készül el, szakképzett, és tapasztalt munkatársaink bizonyosodnak meg róla,
+                                                        hogy az ajánlattételben elvártakból, a legjobbat hozzuk ki (Az ajánlattételről szó esik a "Kínálatunk" oldalunkon).') ?>
                     </div>
                 </div>
-                <div class="row p-5">
+                <div class="row pt-3">
                     <div class="col text-center my-3">
-                        <?php CEO_card("gergo") ?>
+                        <?php info_card("SEO", "Az általunk készített weboldalakat, a Google keresőmotorjához optimalizáljuk,
+                                                így az Önt sokkal hamarabb találják meg a potenciális látogatók.") ?>
                     </div>
                     <div class="col text-center my-3">
-                        <?php CEO_card("zupan") ?>
+                        <?php info_card("Webfejlesztés", "Cégünk fejlesztésért felelős részlege, a design osztályról megérkezett minta alapján, a weoldalt életre kelti.
+                                                            A programozás minden apró kis szegleteiben jártas munkatársaink, ötletes megoldásokkal igyekeznek az Ön weboldalát, mind funkciók,
+                                                            mind multikompatibilitás tekintetében tökéletessé tenni. Programozóink készségei lefedik az egész ipart, kezdve a javascript-től a php-ig.") ?>
                     </div>
+                </div>
+                <div class="col text-center my-3">
+                    <?php CEO_card("gergo") ?>
+                </div>
+                <div class="col text-center my-3">
+                    <?php CEO_card("zupan") ?>
                 </div>
             </div>
         </main>
