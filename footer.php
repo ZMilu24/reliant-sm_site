81651390acb2242311a30518833942e1bea10560
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-05-10 17:56:28 +0200

    uploaded

--- footer.php
+++ footer.php
@@ -3,42 +3,75 @@
 require("config.php");
 
 function footer_fnc($color = "868686") { ?>
-    <footer class="container-fluid p-0" style="background-image: linear-gradient(to left, white, #<?php echo($color); ?>);">
-        <div class="container">
-            <div class="row w-100">
-                <div class="col p-3 px-5">
+    <footer class="container-fluid p-0 text-center" style="background-image: linear-gradient(to left, white, #<?php echo($color); ?>);">
+        <div class="container text-center">
+            <div class="row w-100 text-center">
+                <div class="col p-3 px-5 text-center">
                     <div class="row">
                         <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
                             <h4>
                                 <b id="footer_home">Reliant Service Management</b>
                             </h4>
                         </div>
-                        <!---->
+                        <?php
+                        
+                        require("config.php");
+                        
+                        if($isMob) {
+
+                            ?>
+                    </div>
+                    <div class="row text-center">
                         <div class="col text-center">
-                            <div class="row">
-                                <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
-                                    <div class="row <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
-                                        <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
+                            <div class="row text-center">
+                                <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                    <div class="row <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                        <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
                                             <h4>
                                                 E-mail
                                             </h4>
                                         </div>
                                     </div>
-                                    <div class="row <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
-                                        <div class="col <?php if($isMob) {echo("text-left");} else {echo("text-center");} ?>">
-                                            <p>
-                                                <a id="email_link" onclick=copy(id) class="footer-link text-break">reliant.service.management@gmail.com</a>
+                                    <div class="row <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                        <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                            <p class="text-center">
+                                                <a id="email_link" onclick=copy(id) class="footer-link text-center">reliant<span class="text-break">.</span>service<span class="text-break">.</span>management<span class="text-break">@</span>gmail<span class="text-break">.</span>com</a>
                                             </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                            <?php
+                        } else {
+
+                        ?>
+                        <div class="col text-center">
+                            <div class="row">
+                                <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                    <div class="row <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                        <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                            <h4>
+                                                E-mail
+                                            </h4>
+                                        </div>
+                                    </div>
+                                    <div class="row <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                        <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
+                                            <p class="text-center">
+                                            <a id="email_link" onclick=copy(id) class="footer-link text-center">reliant<span class="text-break">.</span>service<span class="text-break">.</span>management<span class="text-break">@</span>gmail<span class="text-break">.</span>com</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php }; ?>
                     </div>
                 </div>
                 <div class="col p-3 px-5">
                     <div class="row">
-                        <div class="col <?php if($isMob) {echo("text-end");} else {echo("text-center");} ?>">
+                        <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
                             <div class="row">
                                 <div class="col">
                                     <h4>
@@ -55,7 +88,7 @@ function footer_fnc($color = "868686") { ?>
                             </div>
                         </div>
                         <!---->
-                        <div class="col <?php if($isMob) {echo("text-end");} else {echo("text-center");} ?>">
+                        <div class="col <?php if($isMob) {echo("text-center");} else {echo("text-center");} ?>">
                             <img src="imgs/logo2.png" class="rounded-circle" height="100">
                         </div>
                     </div>
