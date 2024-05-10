0e9c6e7447c73a7f4c42311b265f57993cb1617b
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-05-10 17:28:22 +0200

    updated

--- template/premium/index.php
+++ template/premium/index.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="hu">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <title>Premium Template</title>
-        <link href="../template_style.css" rel="stylesheet">
-    </head>
-    <body>
+<?php
+
+    require_once(__DIR__."/../template_config.php");
+
+    template_header("Premium")
+
+?>
         <header class="container-fluid" style="background-image: radial-gradient(closest-side, white, gray);">
             <div class="container p-5">
                 <div class="row">
