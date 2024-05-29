1d4673be5cdd63d8bf09482a45373102b875947f
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-05-29 17:21:34 +0200

    fixing

--- header.php
+++ header.php
@@ -11,8 +11,8 @@
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-                <link href="main_style.css" rel="stylesheet">
-                <link href="style.css" rel="stylesheet">
+                <link href="css/main_style.css" rel="stylesheet">
+                <link href="css/style.css" rel="stylesheet">
                 <?php
                     if ($css != null) {
                         echo('<link href="'.$css.'" rel="stylesheet">');
@@ -245,8 +245,8 @@
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-                <link href="main_style.css" rel="stylesheet">
-                <link href="style.css" rel="stylesheet">
+                <link href="css/main_style.css" rel="stylesheet">
+                <link href="css/style.css" rel="stylesheet">
                 <?php
                     if ($css != null) {
                         echo('<link href="'.$css.'" rel="stylesheet">');
