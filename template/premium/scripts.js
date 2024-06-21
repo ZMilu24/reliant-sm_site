cb2bd9f895922b22b411419790d63b353a742a23
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-06-21 20:00:39 +0200

    premium template

--- template/premium/scripts.js
+++ template/premium/scripts.js
@@ -1,12 +1,17 @@
-document.getElementById('slideButton').addEventListener('click', function() {
-    const container1 = document.getElementById('container1');
-    const container2 = document.getElementById('container2');
-    
-    // Slide container 1 out of view
-    container1.classList.remove('active');
-    
-    // Slide container 2 into view
-    setTimeout(() => {
-        container2.classList.add('active');
-    }, 500); // Delay to match the transition duration
+$(document).ready(function() {
+    let fullHeight = document.documentElement.scrollHeight;
+
+    if (fullHeight !== window.innerHeight) {
+        $('footer').css({
+            'bottom': '0',
+            'width': '100%',
+            'position': 'relative',
+        });
+    } else {
+        $('footer').css({
+            'position': 'absolute',
+            'bottom': '0',
+            'width': '100%',
+        });
+    }
 });
