bff0c3e3ceed841a864fdff95fc218b3089f8d28
ZMilu24 <93102357+ZMilu24@users.noreply.github.com>
2024-06-24 21:46:28 +0200

    header update

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
                 <link href="css/main_style.css" rel="stylesheet">
                 <link href="css/style.css" rel="stylesheet">
+                <link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
                 <?php
                     if ($css != null) {
                         echo('<link href="'.$css.'" rel="stylesheet">');
@@ -259,6 +260,7 @@
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
                 <link href="css/main_style.css" rel="stylesheet">
                 <link href="css/style.css" rel="stylesheet">
+                <link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
                 <?php
                     if ($css != null) {
                         echo('<link href="'.$css.'" rel="stylesheet">');
